7161e9c40e739625db61c4ee9bac942b4ee71a9f
Ritesh Agarwal <ju7stritesh@gmail.com>
2023-02-27 22:45:56 +0530

    Update ped_detect.py

--- ped_detect.py
+++ ped_detect.py
@@ -14,17 +14,12 @@ cap = cv2.VideoCapture("burglar.mp4")
 while(True):
     # Capture frame-by-frame
     ret, image = cap.read()
-    # image = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     image = imutils.resize(image, width=min(400, image.shape[1]))
     orig = image.copy()
     (rects, weights) = hog.detectMultiScale(image, winStride=(4, 4),padding=(8, 8), scale=1.05)
-    # if len(rects) == 0:
-    #     print (type(rects))
     if len(rects) > 0:
         for (x, y, w, h) in rects:
             cv2.rectangle(orig, (x, y), (x + w, y + h), (0, 0, 255), 2)
-            # print (x,y,w,h)
-        # break
     rects = np.array([[x, y, x + w, y + h] for (x, y, w, h) in rects])
     pick = non_max_suppression(rects, probs=None, overlapThresh=0.65)
     for (xA, yA, xB, yB) in pick:
